29563410ad7d75fa52225f0f23e444804954815b
Hans <hansmandersingh1998@gmail.com>
2020-05-20 11:00:45 -0500

    Face up and down is working - not dynamically

--- Stanford Game Project/ContentView.swift
+++ Stanford Game Project/ContentView.swift
@@ -11,17 +11,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         return HStack() {
-            ForEach(0..<4, content: {index in
-                    ZStack() {
-                        RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                        RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                        Text("👻")
-                }
-            })
+            ForEach(0..<4) {index in
+                cardView(isFaceUp: true)
+            }
         }
             .foregroundColor(Color.orange)
             .padding()
             .font(Font.largeTitle)
+    }
+}
+
+struct cardView: View {
+    var isFaceUp : Bool
+    var body: some View {
+        ZStack() {
+            if (isFaceUp) {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+                
+        }
     }
 }
 
