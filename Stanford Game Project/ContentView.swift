2f8fc8d9c4074a3f72bc418a7f589249b2621e3c
Hans <hansmandersingh1998@gmail.com>
2020-05-20 10:20:54 -0500

    Initial File setup with some basic code

---
+++ Stanford Game Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford Game Project
+//
+//  Created by hansmander Singh on 2020-05-20.
+//  Copyright © 2020 hansmander Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10.0).foregroundColor(Color: )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
